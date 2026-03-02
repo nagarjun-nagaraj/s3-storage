973d6cf71f0e78466903816cd9a00b7516c71202
nagarjun-nagaraj <arjuambrose@gmail.com>
2026-03-02 20:25:12 +0530

    feat: add S3 client service

---
+++ app/services/s3.py
@@ -0,0 +1,13 @@
+import boto3
+from app.core.config import(
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_REGION,
+)
+
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION,
+)
