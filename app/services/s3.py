b3df8d8b1e0cc4397a493c19ccd739eba54cd682
nagarjun-nagaraj <arjuambrose@gmail.com>
2026-03-02 20:27:41 +0530

    feat: add upload function to S3 service

--- app/services/s3.py
+++ app/services/s3.py
@@ -1,8 +1,9 @@
 import boto3
-from app.core.config import(
+from app.core.config import (
     AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY,
     AWS_REGION,
+    AWS_BUCKET_NAME,
 )
 
 s3_client = boto3.client(
@@ -11,3 +12,15 @@ s3_client = boto3.client(
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     region_name=AWS_REGION,
 )
+
+
+def upload_file(file, filename: str):
+    s3_client.upload_fileobj(
+        file,
+        AWS_BUCKET_NAME,
+        filename
+    )
+
+    file_url = f"https://{AWS_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{filename}"
+
+    return file_url
